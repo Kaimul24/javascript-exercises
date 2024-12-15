a81d8f2839e611189c987a3b49354adc97da2fdd
Kai <kaimul24@icloud.com>
2024-12-15 13:02:13 -0800

    1-3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let strArr = [...string];
+    strArr = strArr.reverse();
+    reversedString = '';
+    for (let i = 0; i < string.length; i++) {
+        reversedString = reversedString + strArr[i];
+    };
 
+    return reversedString;
+    
 };
 
 // Do not edit below this line
